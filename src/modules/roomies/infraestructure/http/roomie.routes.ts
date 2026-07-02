37d0494b8f69b8e5c61d6f1daa00f5b9197e31d9
esteb <estebantoto9@gmail.com>
2026-07-02 12:01:58 -0500

    feat(roomies): [KAN-104][KAN-105][KAN-106] implements the infraestructure, of messages, adapters, controller and endpoints for the comunication with frontend

--- src/modules/roomies/infraestructure/http/roomie.routes.ts
+++ src/modules/roomies/infraestructure/http/roomie.routes.ts
@@ -11,5 +11,13 @@ router.post('/api/v1/identity/matchmaking-profiles', roomieController.getMatchma
 router.get('/api/v1/identity/check-status/:email', roomieController.checkStatus.bind(roomieController));
 router.post('/api/v1/identity/onboarding', requireKindeAuth, roomieController.onboarding.bind(roomieController));
 router.get('/session', requireKindeAuth, roomieController.checkSession);
+// Endpoint para el botón "Mensaje" de la tarjeta
+router.post('/api/v1/roomies/conversations', roomieController.initializeConversation.bind(roomieController));
+
+// Endpoint para cargar el historial de chats
+router.get('/api/v1/roomies/conversations/:conversationId/messages', roomieController.getHistory.bind(roomieController));
+
+// Endpoint para enviar un mensaje
+router.post('/api/v1/roomies/conversations/:conversationId/messages', roomieController.sendMessage.bind(roomieController));
 
 export default router;
